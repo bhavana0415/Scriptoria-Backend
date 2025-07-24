4095bd2dacfcdc65d90ed598c3570f8fdfecaf98
bhavana0415 <bhavanamanaswi@gmail.com>
2025-07-24 22:52:14 +0530

    removed console.log

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -7,7 +7,6 @@ module.exports = (req, res, next) => {
     return next();
   }
   try {
-    console.log(req.headers.authorization);
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
       throw new Error("Authentication failed!");
@@ -16,7 +15,6 @@ module.exports = (req, res, next) => {
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (err) {
-    console.log(err);
     const error = new HttpError("Authentication failed!", 403);
     return next(error);
   }
